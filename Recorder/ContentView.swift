9c0f60dfb062a51598054bd5df34f17625c023dd
Kristian Primdal <kristian@primux.dk>
2025-09-21 10:20:26 +0800

    Initial Commit

---
+++ Recorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recorder
+//
+//  Created by Kristian Primdal on 21/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recorder/RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecorderApp.swift
+//  Recorder
+//
+//  Created by Kristian Primdal on 21/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
